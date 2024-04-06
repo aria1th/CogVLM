50ca3f201715b97cc0510ab80d6e97e10c251822
AngelBottomless <aria1th@naver.com>
2024-04-07 00:41:41 +0900

    add port to openai_demo

--- openai_demo/openai_api.py
+++ openai_demo/openai_api.py
@@ -151,6 +151,13 @@ async def list_models():
     model_card = ModelCard(id="cogvlm-chat-17b")  # can be replaced by your model id like cogagent-chat-18b
     return ModelList(data=[model_card])
 
+@app.get("/ping")
+async def ping():
+    """
+    An endpoint to list available models. It returns a list of model cards.
+    This is useful for clients to query and understand what models are available for use.
+    """
+    return {"text": "Hello world!", "success" : True}
 
 @app.post("/v1/chat/completions", response_model=ChatCompletionResponse)
 async def create_chat_completion(request: ChatCompletionRequest):
@@ -366,6 +373,11 @@ gc.collect()
 torch.cuda.empty_cache()
 
 if __name__ == "__main__":
+    import argparse
+    # port
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=8000)
+    args = parser.parse_args()
     tokenizer = LlamaTokenizer.from_pretrained(
         TOKENIZER_PATH,
         trust_remote_code=True)
@@ -397,4 +409,4 @@ if __name__ == "__main__":
             
     else:
         model = AutoModelForCausalLM.from_pretrained(MODEL_PATH, trust_remote_code=True).float().to(DEVICE).eval()
-    uvicorn.run(app, host='0.0.0.0', port=8000, workers=1)
+    uvicorn.run(app, host='0.0.0.0', port=args.port, workers=1)
